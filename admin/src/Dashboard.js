c13e0b0bbbcb5ccadf5aa5875c0216d02d200e2c
Nikko <marnichol.sarillana@hcdc.edu.ph>
2023-08-27 20:16:45 +0800

    .

--- admin/src/Dashboard.js
+++ admin/src/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './dashboard.css';
 import './design.css';
 import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
   const location = useLocation();
 
