f67d56b4089dbc8ec972e5c57786f5668a00c839
Nikko <marnichol.sarillana@hcdc.edu.ph>
2023-09-27 18:20:28 +0800

    gi ilisan ug config kay jona na

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyD0jb9GsDjcM_lLlt1glC9PLD-2rha-ecs",
-    authDomain: "admin-eb2c8.firebaseapp.com",
-    projectId: "admin-eb2c8",
-    storageBucket: "admin-eb2c8.appspot.com",
-    messagingSenderId: "767691638099",
-    appId: "1:767691638099:web:af277f64bd753708594da6",
-    measurementId: "G-60NZZCKNGS"
+  apiKey: "AIzaSyDrwvx_GnVZVJp72moA_euWUI_KKUP_kOM",
+  authDomain: "carecabs-26bfa.firebaseapp.com",
+  databaseURL: "https://carecabs-26bfa-default-rtdb.firebaseio.com",
+  projectId: "carecabs-26bfa",
+  storageBucket: "carecabs-26bfa.appspot.com",
+  messagingSenderId: "205073319244",
+  appId: "1:205073319244:web:8cf249e14a7e9e269bfbe9"
   };
